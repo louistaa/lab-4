dfb40389dd4bea750b1985395b63b4a972869fb4
Louis Ta <louistaa@gmail.com>
2021-04-19 14:17:51 -0700

    complete solution

---
+++ index.js
@@ -0,0 +1,14 @@
+let linkToButton = document.querySelector("#callback-button")
+linkToButton.addEventListener('click', function() {
+  console.log("User has clicked the button!")
+})
+
+
+// document.querySelector("#callback-button")
+//     .addEventListener("click", function() {    
+//       console.log("User has clicked on the button!");
+// });
+
+// linkToButton.addEventListener('click', () => {
+//   console.log("Using arrow notation")
+// })
